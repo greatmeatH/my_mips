acf3dca7fcd68e912b11d4d9ee787bff6838fea1
Dragon_Heart_Ubuntu <wojiaozouzixi@gmail.com>
2019-02-25 22:40:35 -0800

    finish

--- Single_Cycle.srcs/sources_1/new/npc.v
+++ Single_Cycle.srcs/sources_1/new/npc.v
@@ -4,8 +4,8 @@ module npc(imm_16,imm_26,pc,npc,NPCSel);
     input [25:0] imm_26;
     input [31:0] pc;
     output [31:0] npc;
-    input NPCSel;
+    input [1:0] NPCSel;
     
-    assign npc=pc+'b100;
+    assign npc=(NPCSel=='b01)?(pc+3'b100+{{14{imm_16[15]}},imm_16,2'b00}):(NPCSel==2'b10)?({pc[31:28],imm_26,2'b00}):(pc+3'b100);
     
 endmodule
